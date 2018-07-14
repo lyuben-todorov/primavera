09c4463bd5e8084a77ae56118f8813868da8ac9a
lyuben <lyuben.todorov@gmail.com>
2018-07-14 20:18:51 +0300

    Finalized auth pages; Begun work on shop modules. TODO: -~auth after register~ -~logout handler~ -~entity framework~? -oauth2 -front-end -core idea
    Signed-off-by: lyuben <lyuben.todorov@gmail.com>
    (cherry picked from commit 2077dcbe1fde835c21819821c28c716623a5bc7b)
    Signed-off-by: lyuben <lyuben.todorov@gmail.com>

--- src/main/java/com/lyuben/primavera/web/ShopController.java
+++ src/main/java/com/lyuben/primavera/web/ShopController.java
@@ -1,16 +1,24 @@
 package com.lyuben.primavera.web;
 
+import com.lyuben.primavera.domain.product.Product;
+import com.lyuben.primavera.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class ShopController {
 
+    @Autowired
+    ProductRepository productRepository;
+
     @GetMapping("/shop")
     public String shop(Model model) {
-        //List<Hookah> hookahList = hookahrep.findAll();
-        //model.addAttribute(hookahList);
+        List<Product> productList = productRepository.findAll();
+        model.addAttribute(productList);
         return "shop";
     }
 }
